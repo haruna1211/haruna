84ca7d55f8da9eb98c2f44bc0b23a69415794928
haruna1211 <144981181+haruna1211@users.noreply.github.com>
2023-09-21 04:45:18 +0000

    おみくじ６

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Streamlitライブラリをインポート
 import streamlit as st
 import random
-st.title('おみくじ')
+st.title('今日の運試し')
 if st.button("おみくじを引く"):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
