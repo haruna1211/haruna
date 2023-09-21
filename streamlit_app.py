7f5d5ca76d28f0f40a198f1d34f4317a5c8e7888
haruna1211 <144981181+haruna1211@users.noreply.github.com>
2023-09-21 23:52:59 +0000

    今日の運勢

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,13 @@ if st.button("今日のラッキーカラー"):
     results=["赤","青","黄色","緑","紫","黒","白"]
     result=random.choice(results)
     st.write(f"結果:{result}")
+
+if st.button("食べると運気が上がるもの"):
+    results["野菜","お肉","魚","豆","果物","乳製品"]
+    result=random.choice(results)
+    st.write(f"結果:{result}")
+
+if st.button("今日は部活があるか"):
+    results["あるに決まってるよね","無いわけなくない","大会近いからもちろんあるよ","甘ったれんな","やらなきゃいけない事たくさんあるよね","休みでもいいけどやる事なかったけ？"]
+    result=random.choice(results)
+    st.write(f"結果:{result}")
