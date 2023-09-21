c428dd3a6ffeef1e14c374491a96d6a16b09725e
haruna1211 <144981181+haruna1211@users.noreply.github.com>
2023-09-21 04:39:04 +0000

    おみくじ３

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ if st.button("おみくじを引く"):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
     st.write(f"結果:{result}")
+
+if st.button("今日のラッキーカラー"):
+    result=["赤","青","黄色"."緑","紫","黒"."白"]
+    result=random.choice(results)
+    st.write(f"結果:{result}")
